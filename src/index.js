8acce4c1f1842b28ada2ff50e8bd4e76672f819e
JoaoFabris <jvfabris21@gmail.com>
2023-01-23 23:44:04 -0300

    Req 2 test

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// não remova esse endpoint, e para o avaliador funcionar!!
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
+
 const talkerPath = path.resolve(__dirname, './talker.json');
 
 const readFile = async () => {
@@ -21,11 +30,6 @@ const readFile = async () => {
   }
 };
 
-// não remova esse endpoint, e para o avaliador funcionar!!
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
-
 app.get('/talker', async (req, res) => {
   try {
     const talker = await readFile();
@@ -35,8 +39,15 @@ app.get('/talker', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await readFile();
+    const findId = data.find((each) => each.id === Number(id));
+    return res.status(HTTP_OK_STATUS).json(findId);
+  } catch (error) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
 });
 
 module.exports = app;
